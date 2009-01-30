47ea8a0de5d2511ebc39151b1d428d5e4985b726
Henrik Rydberg <rydberg@euromail.se>
2009-01-30 13:54:34 +0100

    Removed the AFL.
    Signed-off-by: Henrik Rydberg <rydberg@euromail.se>

--- src/multitouch.c
+++ src/multitouch.c
@@ -3,8 +3,6 @@
  * Multitouch X driver
  * Copyright (C) 2008 Henrik Rydberg <rydberg@euromail.se>
  *
- * Licensed under the Academic Free License version 2.1
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
