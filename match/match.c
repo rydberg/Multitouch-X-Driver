0e711b50d648fd724a9c09da7f1f0317fdeb269f
Henrik Rydberg <rydberg@alnilam.(none)>
2010-01-30 15:03:39 +0100

    License and credits resolved
    All files are tagged as GPL, and a CREDIT file created with
    references to the synaptics X driver and the matching code,
    under their respective MIT and BSD licenses.

--- match/match.c
+++ match/match.c
@@ -1,11 +1,38 @@
+/***************************************************************************
+ *
+ * Multitouch X driver
+ * Copyright (C) 2008 Henrik Rydberg <rydberg@euromail.se>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **************************************************************************/
+
 #include "match.h"
 #include <string.h>
 #include <stdio.h>
 
 /**
- * MATLAB implementation of the hungarian algorithm (2008)
+ * Bitmap implementation of the hungarian algorithm (GPL license)
  *
- * modified by Henrik Rydberg (2008)
+ * Copyright (C) 2008 Henrik Rydberg <rydberg@euromail.se>
+ *
+ * Based on code released by Markus Buehren (2004) (BSD license)
+ *
+ * Copyright (C) 2004, Markus Buehren. All rights reserved.
+ * See CREDITS file for full license terms.
+ *
  */
 
 typedef unsigned short col_t[1];
